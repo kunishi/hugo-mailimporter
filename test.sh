2632ffe8ec07bb6eb84a6f27cb59bf0de6c19bcc
Takeo Kunishima <t.kunishi@gmail.com>
2017-11-10 06:08:07 +0900

    fix a bug in test script.

--- test.sh
+++ test.sh
@@ -10,8 +10,8 @@ for year in 2002 2003 2004 2005 2006 2007 2008 2009 2010 2011 2012 2013 2014; do
   done
 done
 
+rm -f result.txt
 for file in `ls $DBJAPAN_DIR/06_Dbjapan_mlmmj`; do
-  rm -f result.txt
   echo $file | tee -a result.txt
   ./hugo-mailimporter < $DBJAPAN_DIR/06_Dbjapan_mlmmj/$file >> result.txt
 done
